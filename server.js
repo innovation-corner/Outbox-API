fe6329435a8d651099921a99f4af8f90583a211a
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-09-12 19:25:14 +0100

    Add Procfile for Heroku

--- server.js
+++ server.js
@@ -10,9 +10,6 @@ require("./config/passport");
 const port = process.env.PORT || 5000;
 const app = express();
 
-/** building the frontend */
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
 /** set up middlewares */
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,10 +28,6 @@ app.use("/api/v1/book",passport.authenticate('jwt', { session: false }), router.
 app.use("/api/v1/business",passport.authenticate('jwt', { session: false }), router.businessRouter);
 app.use("/api/v1/location",passport.authenticate('jwt', { session: false }), router.locationRouter);
 // app.use("/api/v1/room",passport.authenticate('jwt', { session: false }), router.roomRouter);
-
-app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
-});
 
 /** starting up the server */
 app.listen(port, () => {
